4dc154c8964852508064f1d55ee19596ffb05569
touhidcodes <touhidcodes@gmail.com>
2022-09-04 23:29:07 +0600

    animal calculator

--- animalCalculator.js
+++ animalCalculator.js
@@ -1,2 +1,18 @@
 // count animal
-function animalCalculator(mile) {}
+function animalCalculator(mile) {
+  if (mile <= 10) {
+    var animalCount = mile * 50;
+  } else if (mile > 10 && mile < 20) {
+    var firstCond = 10 * 50;
+    var secondCond = mile - 10;
+    var thirdCond = secondCond * 100;
+    animalCount = firstCond + thirdCond;
+  } else if (mile > 20) {
+    var firstCond = 10 * 50;
+    var secondCond = 10 * 100;
+    var thirdCond = mile - 20;
+    var fourthCond = thirdCond * 300;
+    animalCount = firstCond + secondCond + fourthCond;
+  }
+  return animalCount;
+}
